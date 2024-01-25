e89341bd7f7aceb1875f1ecfe0f0116514e588d2
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-01-25 09:57:19 -0500

    TESTEAR QUE EL NÚMERO DEL GUESS ESTÉ ENTRE 0 Y 1:
    Esta prueba verifica que la suposición generada por la computadora (guess) esté dentro del rango especificado (lowerNumber y higherNumber). Esto es fundamental para asegurar que la lógica de generación de suposiciones esté produciendo resultados válidos dentro de los límites establecidos.
    
    TESTEAR QUE SE AGREGUE UN NÚMERO POR LLAMADA:
    
    Esta prueba verifica que después de cada llamada al método makeGuess, se haya agregado exactamente un número a la lista de suposiciones (guesses). Esto es coherente con la lógica que espera agregar solo una suposición por llamada.
    
    TESTEAR QUE EL GUESS QUE LA COMPUTADORA SUPONGA SEA LA MITAD DEL RANGO:
    
    Esta prueba verifica que la suposición generada por la computadora (guess) sea igual al valor esperado, que es la mitad del rango especificado. Esto asegura que la lógica de makeGuess esté colocando la suposición en el centro del rango.

---
+++ GuessTheNumber/test/ComputerPlayerTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ComputerPlayerTest {
+
+
+    @Test
+    public void testMakeGuess() {
+        int lowerNumber = 0;
+        int higherNumber = 100;
+
+        ComputerPlayer computerPlayer = new ComputerPlayer("Computadora");
+        int guess = computerPlayer.makeGuess(lowerNumber, higherNumber);
+
+        //TESTEAR QUE EL NUMERO DEL GUESS ESTE ENTRE 0 Y 1
+        assertTrue(guess >= lowerNumber && guess <= higherNumber);
+
+        //TESTEAR QUE SE AGREGUE UN NUMERO POR LLAMADA
+        assertEquals(1, computerPlayer.getGuesses().size());
+
+        //TESTEAR QUE EL GUESS QUE LA COMPUTADORA SUPONGA SEA LA MITAD DEL RANGO
+        int expected = lowerNumber + (higherNumber - lowerNumber) / 2;
+        assertEquals(expected, computerPlayer.getGuesses().get(0).intValue());
+    }
+}

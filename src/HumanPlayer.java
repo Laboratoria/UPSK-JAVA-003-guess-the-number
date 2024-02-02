e1b2cb0c12614340f90df6581b8ec1a2087965a9
carolinamoralese <caromorales1396@gmail.com>
2024-02-02 12:23:43 -0500

    se agrega clase que es una subclase de la clase abstracta Player. La clase HumanPlayer representa a un jugador humano en un juego de adivinanza de números

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -8,30 +8,32 @@ public class HumanPlayer extends Player {
         super(name);
         this.scanner = new Scanner(System.in);
     }
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     @Override
     public int makeGuess() {
-        int guess;
+        int guess = 0;
+        boolean inputValid = false;
+
         do {
-            System.out.print( name + " Por favor ingrese su numero entre 1 y 100: ");
-            guess = scanner.nextInt();
+            try {
+                System.out.print(name + " Por favor ingrese su número entre 1 y 100: ");
+                guess = scanner.nextInt();
 
-            if (guess < 1 || guess > 100) {
-                System.out.print("Por favor, ingrese un número válido entre 1 y 100.");
-            }else {
-                scanner.next();
-                System.out.print("Por favor, ingrese un número válido entre 1 y 100.");
+                if (guess < 1 || guess > 100) {
+                    System.out.println("Por favor, ingrese un número válido entre 1 y 100.");
+                } else {
+                    inputValid = true;
+                }
+            } catch (java.util.InputMismatchException e) {
+                scanner.nextLine(); // Limpiar el buffer para evitar un bucle infinito
             }
-        } while (guess < 1 || guess > 100);
-
+        } while (!inputValid);
 
         guesses.add(guess);
         return guess;
-    }
-    public List<Integer> getGuessHistory() {
-        return guesses;
-    }
-    public void setName(String playerName) {
-        this.name = playerName;
     }
 
 }
